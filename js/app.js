fc2ab622ecf773b88e59793eef10beb771b36795
Joy <joylol@users.noreply.github.com>
2015-09-14 11:42:16 -0500

    Add createMarker function on map and change css height and width of map-div.

--- js/app.js
+++ js/app.js
@@ -1,7 +1,46 @@
 var map;
-function initMap() {
-  map = new google.maps.Map(document.getElementById('map-div'), {
-    center: {lat: 33.024819, lng: -96.885055},
-    zoom: 12
+var service;
+var infowindow;
+
+function initialize() {
+  var carrollton = new google.maps.LatLng(33.024819,-96.885055);
+ 	map = new google.maps.Map(document.getElementById('map-div'), {
+   		center: carrollton,
+    	zoom: 13
+  });
+
+	var request = {
+		location: carrollton,
+		radius: '500',
+		query: 'park'
+	};
+
+  infowindow = new google.maps.InfoWindow();
+
+  service = new google.maps.places.PlacesService(map);
+  service.textSearch(request, callback);
+}
+
+function callback(results, status) {
+  if (status == google.maps.places.PlacesServiceStatus.OK) {
+    for (var i = 0; i < results.length; i++) {
+      //var place = results[i];
+      createMarker(results[i]);
+    }
+  }
+}
+
+function createMarker(place) {
+  var placeLoc = place.geometry.location;
+  var marker = new google.maps.Marker({
+    map: map,
+    position: place.geometry.location
+  });
+
+  google.maps.event.addListener(marker, 'click', function() {
+    infowindow.setContent(place.name);
+    infowindow.open(map, this);
   });
 }
+
+initialize();
