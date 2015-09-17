bcd678afe0dc9c6b3236a5a3d166191db79f700f
Joy <joylol@users.noreply.github.com>
2015-09-17 11:55:22 -0500

    Change search bar and css related to search bar, hard-code favorite places in an array of objects, and setMarkers on google map.

--- js/app.js
+++ js/app.js
@@ -1,46 +1,57 @@
-var map;
-var service;
-var infowindow;
+var favoritePlaces = [
+  {
+    name: 'Arbor Hills Nature Preserve',
+    lat: 33.048430,
+    lng: -96.851033,
+    text: 'A beautiful park'
+  },
+  {
+    name: 'Sprouts',
+    lat: 33.025549,
+    lng: -96.888284,
+    text: 'A small grocery store with lots of natural and organic items'
+  },
+  {
+    name: 'Carrollton Public Library',
+    lat: 33.026226,
+    lng: -96.883365,
+    text: 'A modern neighborhood public library'
+  },
+  {
+    name: 'HMart',
+    lat: 32.984694,
+    lng: -96.912247,
+    text: 'A Korean Asian grocery store or supermarket'
+  },
+  {
+    name: 'The Shops at Willow Bend',
+    lat: 33.030992,
+    lng: -96.832434,
+    text: 'A clean, uncrowded, upscale mall'
+  }
+];
 
-function initialize() {
+var map;
+
+function initMap() {
   var carrollton = new google.maps.LatLng(33.024819,-96.885055);
- 	map = new google.maps.Map(document.getElementById('map-div'), {
-   		center: carrollton,
-    	zoom: 13
+  map = new google.maps.Map(document.getElementById('map'), {
+      center: carrollton,
+      zoom: 13
   });
 
-	var request = {
-		location: carrollton,
-		radius: '500',
-		query: 'park'
-	};
-
-  infowindow = new google.maps.InfoWindow();
-
-  service = new google.maps.places.PlacesService(map);
-  service.textSearch(request, callback);
+  setMarkers(map);
 }
 
-function callback(results, status) {
-  if (status == google.maps.places.PlacesServiceStatus.OK) {
-    for (var i = 0; i < results.length; i++) {
-      //var place = results[i];
-      createMarker(results[i]);
-    }
+function setMarkers(map) {
+  for(var i = 0; i < favoritePlaces.length; i++) {
+    var favoritePlace = favoritePlaces[i];
+    var marker = new google.maps.Marker({
+      position: {lat: favoritePlace.lat, lng: favoritePlace.lng},
+      map: map,
+      title: favoritePlace.name
+    });
   }
 }
 
-function createMarker(place) {
-  var placeLoc = place.geometry.location;
-  var marker = new google.maps.Marker({
-    map: map,
-    position: place.geometry.location
-  });
-
-  google.maps.event.addListener(marker, 'click', function() {
-    infowindow.setContent(place.name);
-    infowindow.open(map, this);
-  });
-}
-
-initialize();
+initMap();
